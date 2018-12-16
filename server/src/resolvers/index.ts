e8f96c9ee2af253df0efc46e72139fdd823c8228
Aymerick Valette <valaymerick@gmail.com>
2018-12-16 19:00:11 +0100

    Add updateWorkspace resolver

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -10,36 +10,47 @@ export default {
         createUser(root, args, context) {
             return context.prisma.createUser(
                 {
-                    email: args.email,
-                    skills: {
-                        create: args.skills.map(skill => ({name : skill}))
-                    }
+                    email: args.email
                 }
-
             )
         },
         createWorkspace(root, args, context) {
             return context.prisma.createWorkspace(
                 {
                     name: args.name,
+                    users: [args.administrator],
                     administrator: {
                         connect: {
-                            id: args.administrator
+                            email: args.administrator
                         }
                     },
+                }
+            )
+        },
+        updateWorkspace(root, args, context) {
+            return context.prisma.updateWorkspace(
+                {
+                    data: {
+                        name: args.name,
+                        users: args.users,
+                        teams: args.teams
+                    },
+                    where: {
+                        id: args.id
+                    }
                 }
             )
         }
     },
     User: {
         skills(parent) {
-            return prisma.user({id: parent.id}).skills()
+            return prisma.user({ id: parent.id }).skills()
         }
     },
 
     Skill: {
         users(parent) {
-            return prisma.skill({name: parent.name}).users()
+            return prisma.skill({ name: parent.name }).users()
         }
     }
 }
