048d690f2ef27a20351adba79b4894fc9075d737
Aymerick Valette <valaymerick@gmail.com>
2019-01-08 13:57:10 +0100

    Init client and use the Polaris design system

---
+++ client/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { DisplayText } from '@shopify/polaris'
+import '@shopify/polaris/styles.css'
+
+ReactDOM.render(<DisplayText size="extraLarge">Welcome to Teamy</DisplayText>, document.getElementById('root'))
