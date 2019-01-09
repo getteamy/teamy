8f785aa099fcd77aa0b91b40b0d310d9526776bb
Aymerick Valette <valaymerick@gmail.com>
2019-01-09 14:04:06 +0100

    Struture the login layout and add Logo

--- client/src/views/Register.tsx
+++ client/src/views/Register.tsx
@@ -1,31 +1,58 @@
 import React from 'react'
-import { Button, ButtonGroup, Card, Form, FormLayout, Layout, TextField, AppProvider, Page } from '@shopify/polaris'
+import { Button, ButtonGroup, ActionList, Popover, Card, Form, FormLayout, Layout, TextField, AppProvider, Page } from '@shopify/polaris'
+import styled from 'styled-components'
+
+const MainLayout = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`
+
+const Logo = styled.img`
+    width: 100%;
+    height: 128px;
+    object-fit: contain;
+`
+
+const LoginWrapper = styled.div`
+    width: 384px;
+    margin-bottom: 128px;
+`
 
 const Register = () => (
     <AppProvider>
-        <Page
-            title="Welcome to Teamy"
-        >
-            <Layout>
-                <Layout.AnnotatedSection
-                    title="Register"
-                    description="Teamy lets you build better, faster and stronger teams with strong people matching algorithms"
-                >
-                    <Card sectioned>
+        <MainLayout>
+            <LoginWrapper>
+                <Logo src={"https://i.imgur.com/QflCUhS.png"}></Logo>
+                <Card sectioned>
+                    <Card.Header
+                        title="Welcome to Teamy"
+                    >
+                        <Popover
+                            active={false}
+                            activator={
+                                <Button disclosure plain>
+                                    Login
+                            </Button>
+                            }
+                            onClose={() => { }}
+                        >
+                            <ActionList items={[{ content: 'Login' }, { content: 'Register' }]} />
+                        </Popover>
+                    </Card.Header>
+                    <Card.Section>
                         <Form onSubmit={() => console.log("hey")}>
                             <FormLayout>
                                 <TextField label="Email" type="email" placeholder="e.g. johndoe@example.com" onChange={(e => e)} />
                                 <TextField type="password" label="Password" onChange={(e => e)} />
-                                <ButtonGroup>
-                                    <Button size="large" primary submit>Register</Button>
-                                    <Button plain>.. or login</Button>
-                                </ButtonGroup>
+                                <Button primary size={"large"} fullWidth submit>Login</Button>
                             </FormLayout>
                         </Form>
-                    </Card>
-                </Layout.AnnotatedSection>
-            </Layout>
-        </Page>
+                    </Card.Section>
+                </Card>
+            </LoginWrapper>
+        </MainLayout>
     </AppProvider>
 )
 
