51059a5b66fe7f7fcb03c60ecb0a881887f2256a
Aymerick Valette <valaymerick@gmail.com>
2019-03-02 18:32:33 +0100

    Add shadows constants

---
+++ client/src/utils/shadows.ts
@@ -0,0 +1,8 @@
+export const shadows = {
+    80: '0 1px 4px 0 rgba(0, 0, 0, 0.12)',
+    100: '0 2px 4px -2px rgba(0, 0, 0, 0.2)',
+    150: '0 3px 6px -2px rgba(0, 0, 0, 0.2)',
+    200: '0 7px 14px -4px rgba(0, 0, 0, 0.14)',
+    300: '0 12px 24px -6px rgba(0, 0, 0, 0.14)',
+    400: '0 22px 30px -8px rgba(0, 0, 0, 0.14)',
+  }
