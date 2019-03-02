8b8d287941ff21e6eb59dd315a56d8c6980a3dd0
Aymerick Valette <valaymerick@gmail.com>
2019-03-02 18:32:42 +0100

    Add subdued property to texts

--- client/src/components/Typography/index.tsx
+++ client/src/components/Typography/index.tsx
@@ -1,65 +1,96 @@
 import styled from 'styled-components'
+import { colors } from '../../utils/colors'
 
-export const H6 = styled.p`
+interface TextProps {
+    subdued?: boolean
+}
+
+const getSubduedColor = (subdued? : boolean) => subdued ? colors.inkLighter : colors.ink
+
+export const H6 = styled.p<TextProps>`
     opacity: 0.87;
     font-size: 20px;
     letter-spacing: 0.15px;
+
+    color: ${({subdued}) => getSubduedColor(subdued)}
 `
-export const H5 = styled.p`
+export const H5 = styled.p<TextProps>`
     opacity: 0.87;
     font-size: 24px;
+
+    color: ${({subdued}) => getSubduedColor(subdued)}
 `
-export const H4 = styled.p`
+export const H4 = styled.p<TextProps>`
     opacity: 0.87;
     font-size: 34px;
     letter-spacing: 0.25px;
     text-align: left;
+
+    color: ${({subdued}) => getSubduedColor(subdued)}
 `
-export const H3 = styled.p`
+export const H3 = styled.p<TextProps>`
     opacity: 0.87;
     font-size: 48px;
     text-align: left;
+    
+    color: ${({subdued}) => getSubduedColor(subdued)}
 `
-export const H2 = styled.p`
-    opacity: 0.87;font-size: 60px;
+export const H2 = styled.p<TextProps>`
+    opacity: 0.87;
+    font-size: 60px;
     letter-spacing: -0.5px;
     text-align: left;
+
+    color: ${({subdued}) => getSubduedColor(subdued)}
 `
-export const H1 = styled.p`
-    opacity: 0.87;font-size: 96px;
+export const H1 = styled.p<TextProps>`
+    opacity: 0.87;
+    font-size: 96px;
     letter-spacing: -1.5px;
     text-align: left;
+
+    color: ${({subdued}) => getSubduedColor(subdued)}
 `
 
-export const Subtitle = styled.p`
+export const Subtitle = styled.p<TextProps>`
     opacity: 0.87;
     font-size: 16px;
-    color: rgba(0,0,0,0.60);
     letter-spacing: 0;
     text-align: left;
     line-height: 24px;
+    margin: 0 10px;
+
+    color: ${({subdued}) => getSubduedColor(subdued)}
 `
 
-export const SmallSubtitle = styled.p`
+export const SmallSubtitle = styled.p<TextProps>`
     opacity: 0.87;
     font-size: 14px;
-    color: rgba(0,0,0,0.60);
     letter-spacing: -0.13px;
     text-align: left;
     line-height: 24px;
+    margin: 0 10px;
+
+    color: ${({subdued}) => getSubduedColor(subdued)}
 `
 
-export const Body = styled.p`
+export const Body = styled.p<TextProps>`
     font-size: 16px;
     letter-spacing: 0;
     text-align: left;
     line-height: 28px;
+    margin: 0 10px;
+
+    color: ${({subdued}) => getSubduedColor(subdued)}
 `
 
-export const SmallBody = styled.p`
+export const SmallBody = styled.p<TextProps>`
     opacity: 0.87;
     font-size: 14px;
     letter-spacing: 0;
     text-align: left;
     line-height: 20px;
+    margin: 0 10px;
+
+    color: ${({subdued}) => getSubduedColor(subdued)}
 `
