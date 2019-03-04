aa24c8d290ef90980f5a0eecbedf9b1edd56823f
Aymerick Valette <valaymerick@gmail.com>
2019-03-04 23:57:18 +0100

    Add error link

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import ApolloClient, { InMemoryCache } from 'apollo-boost'
+import { onError, ErrorResponse } from 'apollo-link-error'
 import { ApolloProvider } from 'react-apollo'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -10,8 +11,14 @@ import './reset.css'
 
 import Auth from './views/Auth'
 
+const errorLink = ({ graphQLErrors, networkError } : ErrorResponse) => {
+    if (graphQLErrors) graphQLErrors.map(({ message, locations, path }) => console.log(message))
+    if (networkError) console.log(`[Network error]: ${networkError}`)
+}
+
 const client = new ApolloClient({
     cache: new InMemoryCache(),
+    onError: errorLink,
     uri: 'http://localhost:4000',
 })
 
