127eb9a84b00ad0d81532e0e7ed5e874a5874369
Aymerick Valette <valaymerick@gmail.com>
2019-03-31 21:02:33 +0200

    Display only GraphQL errors

--- packages/client/src/views/Auth/Register.tsx
+++ packages/client/src/views/Auth/Register.tsx
@@ -12,80 +12,92 @@ import Logo from '../../components/Logo'
 import PasswordStrengthMeter from '../../components/PasswordStrengthMeter'
 import StyledInput from '../../components/TextField'
 import { H200, H600, Link, Paragraph } from '../../components/Typography'
-import { CardContainer, Container, Footer, Header, StyledForm, UnderlineContainer } from './style'
+import {
+  CardContainer,
+  Container,
+  Footer,
+  Header,
+  StyledForm,
+  UnderlineContainer
+} from './style'
 
 const REGISTER = gql`
-    mutation register($name: String!, $password: String!) {
-        signup(name: $name, password: $password) {
-            id
-        }
+  mutation register($name: String!, $password: String!) {
+    signup(name: $name, password: $password) {
+      id
     }
+  }
 `
 
 function Register() {
-    const [name, setName] = useState('')
-    const [password, setPassword] = useState('')
-    const [passwordStrength, setPasswordStrength] = useState(0)
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordStrength, setPasswordStrength] = useState(0)
 
-    const isValid = (name !== '' && passwordStrength > 1)
+  const isValid = name !== '' && passwordStrength > 1
 
-    useEffect(
-        () => {
-            const strength = Number(zxcvbn(password).score.toFixed())
-            setPasswordStrength(strength)
-        }, [password],
-    )
+  useEffect(() => {
+    const strength = Number(zxcvbn(password).score.toFixed())
+    setPasswordStrength(strength)
+  }, [password])
 
-    return (
-        <Container>
-            <Logo />
-            <CardContainer>
-                <Mutation mutation={REGISTER}>
-                    {(submit, { data, loading, error }) =>
-                        <Card>
-                            <Header>
-                                <H600>Welcome</H600>
-                                <H200>Register to Teamy</H200>
-                            </Header>
-                            <StyledForm onKeyDown={({key}) => key === 'Enter' && isValid && submit({ variables: { name, password } })}>
-                                {error && <FormError message={error.graphQLErrors[0].message}/>}
-                                <StyledInput
-                                    label='Name'
-                                    onChange={({ target: { value } }: any) => setName(value)}
-                                    value={name}
-                                    placeholder='johndoe@example.com'
-                                />
-                                <StyledInput
-                                    label='Password'
-                                    type='password'
-                                    onChange={({ target: { value } }: any) => setPassword(value)}
-                                    value={password}
-                                />
-                            </StyledForm>
-                            <Footer>
-                                <PasswordStrengthMeter 
-                                    disabled={password === ''}
-                                    strength={passwordStrength}
-                                />
-                                <Button
-                                    isLoading={loading}
-                                    onClick={() => submit({ variables: { name, password } })}
-                                    isDisabled={!isValid}
-                                >
-                                    Register
-                                </Button>
-                            </Footer>
-                        </Card>}
-                </Mutation>
-            </CardContainer>
-            <UnderlineContainer>
-                <Paragraph>
-                    First time there?
-                </Paragraph>
-                <Link to='/login'>Connect to your account</Link>
-            </UnderlineContainer>
-        </Container>
-    )
+  return (
+    <Container>
+      <Logo />
+      <CardContainer>
+        <Mutation mutation={REGISTER}>
+          {(submit, { data, loading, error }) => (
+            <Card>
+              <Header>
+                <H600>Welcome</H600>
+                <H200>Register to Teamy</H200>
+              </Header>
+              <StyledForm
+                onKeyDown={({ key }) =>
+                  key === 'Enter' &&
+                  isValid &&
+                  submit({ variables: { name, password } })
+                }
+              >
+                {error && error.graphQLErrors[0] && (
+                  <FormError message={error.graphQLErrors[0].message} />
+                )}
+                <StyledInput
+                  label="Name"
+                  onChange={({ target: { value } }: any) => setName(value)}
+                  value={name}
+                  placeholder="johndoe@example.com"
+                />
+                <StyledInput
+                  label="Password"
+                  type="password"
+                  onChange={({ target: { value } }: any) => setPassword(value)}
+                  value={password}
+                />
+              </StyledForm>
+              <Footer>
+                <PasswordStrengthMeter
+                  disabled={password === ''}
+                  strength={passwordStrength}
+                />
+                <Button
+                  isLoading={loading}
+                  onClick={() => submit({ variables: { name, password } })}
+                  isDisabled={!isValid}
+                >
+                  Register
+                </Button>
+              </Footer>
+            </Card>
+          )}
+        </Mutation>
+      </CardContainer>
+      <UnderlineContainer>
+        <Paragraph>First time there?</Paragraph>
+        <Link to="/login">Connect to your account</Link>
+      </UnderlineContainer>
+    </Container>
+  )
 }
 
 export default Register
