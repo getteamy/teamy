923ad2d1c9c6dbc1a9af460bdb5d6de35b5059af
Aymerick Valette <valaymerick@gmail.com>
2019-03-31 21:02:09 +0200

    Show only graphQL errors on forms

--- packages/client/src/views/Auth/Login.tsx
+++ packages/client/src/views/Auth/Login.tsx
@@ -10,69 +10,79 @@ import Logo from '../../components/Logo'
 import StyledInput from '../../components/TextField'
 import { H200, H600, Link, Paragraph } from '../../components/Typography'
 import { variations } from '../../utils/variations'
-import { CardContainer, Container, Footer, Header, StyledForm, UnderlineContainer } from './style'
+import {
+  CardContainer,
+  Container,
+  Footer,
+  Header,
+  StyledForm,
+  UnderlineContainer
+} from './style'
 
 const LOGIN = gql`
-    mutation login($name: String!, $password: String!) {
-        login(name: $name, password: $password) {
-            token
-        }
+  mutation login($name: String!, $password: String!) {
+    login(name: $name, password: $password) {
+      token
     }
+  }
 `
 
 function Login() {
-    const [name, setName] = useState('')
-    const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
 
-    const isValid = (password !== '' || name !== '')
+  const isValid = password !== '' || name !== ''
 
-    return (
-        <Container>
-            <Logo />
-            <CardContainer>
-                <Mutation mutation={LOGIN}>
-                    {(submit, { data, loading, error }) =>
-                        <Card>
-                            <Header>
-                                <H600>Welcome</H600>
-                                <H200>Connect to Teamy</H200>
-                            </Header>
-                            <StyledForm>
-                                {error && error.graphQLErrors[0] && <FormError message={error.graphQLErrors[0].message}/>}
-                                <StyledInput
-                                    label='Name'
-                                    onChange={({ target: { value } }: any) => setName(value)}
-                                    value={name}
-                                    placeholder='johndoe@example.com'
-                                />
-                                <StyledInput
-                                    label='Password'
-                                    type='password'
-                                    onChange={({ target: { value } }: any) => setPassword(value)}
-                                    value={password}
-                                />
-                            </StyledForm>
-                            <Footer>
-                                <Button variation={variations.LINK}>I forgot my password</Button>
-                                <Button
-                                    isLoading={loading}
-                                    onClick={() => submit({ variables: { name, password } })}
-                                    isDisabled={!isValid}
-                                >
-                                    Login
-                                </Button>
-                            </Footer>
-                        </Card>}
-                </Mutation>
-            </CardContainer>
-            <UnderlineContainer>
-                <Paragraph>
-                    First time there?
-                </Paragraph>
-                <Link to='/register'>Create an account</Link>
-            </UnderlineContainer>
-        </Container>
-    )
+  return (
+    <Container>
+      <Logo />
+      <CardContainer>
+        <Mutation mutation={LOGIN}>
+          {(submit, { data, loading, error }) => (
+            <Card>
+              <Header>
+                <H600>Welcome</H600>
+                <H200>Connect to Teamy</H200>
+              </Header>
+              <StyledForm>
+                {error && error.graphQLErrors[0] && (
+                  <FormError message={error.graphQLErrors[0].message} />
+                )}
+                <StyledInput
+                  label="Name"
+                  onChange={({ target: { value } }: any) => setName(value)}
+                  value={name}
+                  placeholder="johndoe@example.com"
+                />
+                <StyledInput
+                  label="Password"
+                  type="password"
+                  onChange={({ target: { value } }: any) => setPassword(value)}
+                  value={password}
+                />
+              </StyledForm>
+              <Footer>
+                <Button variation={variations.LINK}>
+                  I forgot my password
+                </Button>
+                <Button
+                  isLoading={loading}
+                  onClick={() => submit({ variables: { name, password } })}
+                  isDisabled={!isValid}
+                >
+                  Login
+                </Button>
+              </Footer>
+            </Card>
+          )}
+        </Mutation>
+      </CardContainer>
+      <UnderlineContainer>
+        <Paragraph>First time there?</Paragraph>
+        <Link to="/register">Create an account</Link>
+      </UnderlineContainer>
+    </Container>
+  )
 }
 
 export default Login
