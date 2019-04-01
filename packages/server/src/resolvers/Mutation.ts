d29f635672f591a58de4a4299f55d3b05d6ab86d
Aymerick Valette <valaymerick@gmail.com>
2019-04-02 00:49:45 +0200

    Lint

--- packages/server/src/resolvers/Mutation.ts
+++ packages/server/src/resolvers/Mutation.ts
@@ -4,48 +4,48 @@ import * as jwt from 'jsonwebtoken'
 const APP_SECRET = 'secret'
 
 const signup = async (parent, { name, password }, context, info) => {
-    const exists = await context.prisma.user({ name })
+  const exists = await context.prisma.user({ name })
 
-    if (exists) {
-        throw new Error('User already exists')
-    }
-    
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const user = await context.prisma.createUser({
-        name,
-        password: hashedPassword
-    })
-    return user
+  if (exists) {
+    throw new Error('User already exists')
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const user = await context.prisma.createUser({
+    name,
+    password: hashedPassword
+  })
+  return user
 }
 
-const login = async (parent, { name, password }, context, info) => {
-    const user = await context.prisma.user({ name })
+const login = async (parent, { name, password }, context) => {
+  const user = await context.prisma.user({ name })
 
-    if (!user) {
-        throw new Error("User doesn't exist")
+  if (!user) {
+    throw new Error('User doesn\'t exists')
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password)
+
+  if (!passwordMatch) {
+    throw new Error('Password doesn\'t match')
+  }
+
+  const token = jwt.sign(
+    {
+      id: user.id,
+      username: user.name
+    },
+    APP_SECRET,
+    {
+      expiresIn: '30d'
     }
+  )
 
-    const passwordMatch = await bcrypt.compare(password, user.password)
-
-    if (!passwordMatch) {
-        throw new Error("Password doesn't match")
-    }
-
-    const token = jwt.sign(
-        {
-            id: user.id,
-            username: user.name
-        },
-        APP_SECRET,
-        {
-            expiresIn: '30d',
-        }
-    )
-
-    return {
-        token,
-        user,
-    }
+  return {
+    token,
+    user
+  }
 }
 
 export default { signup, login }
