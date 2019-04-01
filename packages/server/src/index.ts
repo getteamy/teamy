5805243c807337637e740758723aec7b5055dede
Aymerick Valette <valaymerick@gmail.com>
2019-04-01 14:58:22 +0200

    Avoid uncessary complexity

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -7,35 +7,31 @@ import Mutation from './resolvers/Mutation'
 import Query from './resolvers/Query'
 
 const resolvers = {
-    Mutation,
-    Query,
+  Mutation,
+  Query
 }
 
-function getUser(token) {
-    try {
-        if (token) {
-            return jwt.verify(token, "secret")
-        }
-        return null
-    }
-    catch (error) {
-        return null
-    }
+function getUser(token: string) {
+  if (token) {
+    return jwt.verify(token, 'secret')
+  }
+  return null
 }
 
 const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers,
-    context: ({ request }) => {
-        const tokenWithBearer = request.headers.authorization || ''
-        const token = tokenWithBearer.split(' ')[1]
-        const user = getUser(token)
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context({ request }) {
+    const tokenWithBearer = request.headers.authorization || ''
+    const token = tokenWithBearer.split(' ')[1]
+    const user = getUser(token)
 
-        return {
-            user,
-            prisma
-        }
-    },
+    return {
+      user,
+      prisma
+    }
+  }
 })
 
+//@eslint:ignore
 server.start(() => console.log('👨‍💻️ Server is running on port 4000'))
